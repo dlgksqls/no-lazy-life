a2ab707af5605d666dc912182ddf2edcba0d8451
dlgksqls <horainlee87@gmail.com>
2025-07-10 15:21:42 +0900

    20250710

---
+++ HanBin/sol_2025_07/boj10971.java
@@ -0,0 +1,86 @@
+package sol_2025_07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+/**
+ * 외판원 순회 => 시작점부터 출발해서 여러 도시를 거쳐서 다시 출발점으로 돌아온다.
+ * ex) n == 5
+ * 1 -> 2 -> 3 -> 4 -> 5 -> 1
+ *
+ * 출발은 어디에서 하든 상관없다. 하지만 외판원 순회가 끝났을 때 가장 최소의 값을 지불할 때의 값
+ *
+ * recursion() 재귀를 통해 history (방문한 도시)를 채운다.
+ * 재귀 한 후에는 모든 도시를 순회 (순열) 하기위해 history.remove(history.size() - 1)을 해준다.
+ * 재귀의 깊이 (depth) == city.length 가 됐다면 모든 도시를 순회한 것이므로 값을 계산한다
+ * 여기서 도시간의 거리가 0이라면 갈 수 없다는 뜻이므로 return 을 해준다.
+ *
+ * 마지막에 마지막 도시에서 시작 도시로 가는 값을 더해준다.
+ * 여기서도 역시 거리가 0이라면 그냥 return 한다.
+ */
+
+public class boj10971 {
+    static ArrayList<Integer>[] city;
+    static ArrayList<Integer> history;
+    static boolean[] visited;
+    static int answer = Integer.MAX_VALUE;
+    static int n;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        city = new ArrayList[n];
+        for(int i=0; i<n; i++){
+            city[i] = new ArrayList<>();
+        }
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++) {
+                int value = Integer.parseInt(st.nextToken());
+                city[i].add(value);
+            }
+        }
+
+        visited = new boolean[n];
+        history = new ArrayList<>();
+        recursion(0);
+
+        System.out.println(answer);
+        br.close();
+    }
+
+    private static void recursion(int depth) {
+        if (depth == city.length){
+            int val = 0;
+            int now = history.get(0);
+            for(int i=1; i<history.size(); i++){
+                int cost = city[now].get(history.get(i));
+                if (cost == 0) return; // 길이 없다면 return
+                val += cost;
+                now = history.get(i);
+            }
+
+            int returnCost = city[now].get(history.get(0));
+            if (returnCost == 0) return; // 돌아오는 길이 없다면 return
+            val += returnCost;
+            answer = Math.min(answer, val);
+            return;
+        }
+
+        for(int i=0; i<n; i++){
+            if (!visited[i]){
+                visited[i] = true;
+                history.add(i);
+                recursion(depth + 1);
+                visited[i] = false;
+                history.remove(history.size()-1);
+            }
+        }
+    }
+}
---
+++ HanBin/sol_2025_07/boj14712.java
@@ -0,0 +1,103 @@
+package sol_2025_07;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * boolean[][] array를 채우는데, 채운 것이 연속으로 2 * 2가 되면 안된다
+ * ex) 2 * 2 array
+ *
+ * O O => 불가능  O X => 가능
+ * O O          O O
+ *
+ * 재귀를 사용한다.
+ * 첫번째 방법
+ * recursion() 메서드 : array의 처음부터 시작하여 (0, 0) 1. 다음 칸에 네모를 채우지 않는 방법, 2. 다음칸을 검사하여 2 * 2가 만족하지 않는다면 다음 칸을 채우는 방법
+ * x == n (마지막 칸까지 조사한 후 다음 칸 x => n) 이 되면 이 재귀는 가능한 재귀라는 뜻이므로 ++
+ *
+ * 두번째 방법 (오래 걸림)
+ * recursion() 메서드 : idx를 사용하여 x = idx / m, y = idx % m 이라는 것을 이용하여
+ * 1. 다음 칸에 네모를 채우지 않는 방법, 2. 채우는 방법
+ * 이렇게 하여 네모를 채우는 모든 경우의 수를 구한다.
+ * 그 후 idx = n * m 이면 모든 칸을 다 조사한 것이기에 검사를 진행한다.
+ */
+public class boj14712 {
+    static int n, m;
+    static boolean[][] array;
+    static int answer = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        array = new boolean[n][m];
+        recursion(0, 0);
+//        recursion(0);
+
+        System.out.println(answer);
+        br.close();
+    }
+
+    private static void recursion(int x, int y) {
+        if (x == n){
+            answer ++;
+            return;
+        }
+
+        int nextX = x;
+        int nextY = y+1;
+
+        if (nextY == m){
+            nextX += 1;
+            nextY = 0;
+        }
+
+        recursion(nextX, nextY);
+
+        array[x][y] = true;
+        if (!check(x, y)){
+            recursion(nextX, nextY);
+        }
+        array[x][y] = false;
+    }
+
+    private static boolean check(int x, int y) {
+        if (x >= 1 && y >= 1){
+            return array[x-1][y-1] && array[x-1][y] && array[x][y-1];
+        }
+        return false;
+    }
+
+//    private static void recursion(int idx){
+//        if (idx == n * m){
+//            if (!check()) answer ++;
+//            return;
+//        }
+//
+//        int x = idx / m;
+//        int y = idx % m;
+//
+//        recursion(idx + 1);
+//
+//        array[x][y] = true;
+//        recursion(idx + 1);
+//        array[x][y] = false;
+//    }
+//
+//    private static boolean check(){
+//        for(int i=1; i<n; i++){
+//            for(int j=1; j<m; j++){
+//                if (array[i][j]) {
+//                    if (array[i - 1][j - 1] && array[i - 1][j] && array[i][j - 1] && array[i][j]) return true;
+//                }
+//            }
+//        }
+//        return false;
+//    }
+//
+}
